ab703cbccc136e7e7d79d48aefd4aa1d9b00a84c
AlejandraMendivels <larairan@uniboyaca.edu.co>
2023-06-06 23:28:19 -0500

    Se agrego funcionalidades a actividades de modificar ,agregar y eliminar

---
+++ js/actividades.js
@@ -0,0 +1,119 @@
+let indexActividad = 0;
+
+$(document).ready(function () {
+    mostrarActividades();
+});
+
+function mostrarActividades() {
+    $.ajax({
+        method: 'get',
+        url: 'http://localhost:8000/miraractividades'
+    }).done((response) => {
+        const dataJson = JSON.parse(response);
+        const actividades = dataJson.data;
+        const table = document.getElementById('actividadesTb');
+        const tbody = table.getElementsByTagName('tbody')[0];
+        let html = '';
+        let notas = [];
+
+        actividades.forEach(actividad => {
+            html += '<tr>';
+            html += '   <td>' + actividad.id + '</td>';
+            html += '   <td>' + actividad.descripcion + '</td>';
+            html += '   <td>' + actividad.nota + '</td>';
+            html += '   <td>';
+            html += '      <button onclick="modificar(' + actividad.id + ')">Modificar</button>';
+            html += '   </td>';
+            html += '   <td>';
+            html += '      <button onclick="eliminar(' + actividad.id + ')">Eliminar</button>';
+            html += '   </td>';
+            html += '</tr>';
+
+            notas.push(parseFloat(actividad.nota));
+        });
+
+        const totalNotas = notas.reduce((sum, nota) => sum + nota, 0);
+        const promedio = totalNotas / notas.length;
+
+        html += '<tr>';
+        html += '   <td colspan="2">Promedio de notas:</td>';
+        html += '   <td>' + promedio.toFixed(2) + '</td>';
+        html += '   <td colspan="2">' + (promedio > 3 ? 'Aprobó' : 'Reprobó') + '</td>';
+        html += '</tr>';
+
+        tbody.innerHTML = html;
+    }).fail((error) => {
+        console.error(error);
+    });
+}
+
+
+document.getElementById('registrar').addEventListener('click', () => {
+    indexActividad = -1;
+    document.getElementById('tituloModal').innerText = 'Registrar';
+});
+
+document.getElementById('guardar').addEventListener('click', () => {
+    let formulario = document.forms['formularioActividad'];
+    let descripcion = formulario['descripcion'].value;
+    let nota = formulario['nota'].value;
+    let codigoEstudiante = formulario['codigoEstudiante'].value;
+
+
+    if (indexActividad == -1) {
+        $.ajax({
+            url: 'http://localhost:8000/crearactividades',
+            method: 'post',
+            data: {
+                descripcion: descripcion,
+                nota: nota,
+                codigoEstudiante: codigoEstudiante
+            }
+        }).done(response => {
+            const dataJson = JSON.parse(response);
+            const msg = dataJson.data;
+            alert(msg);
+            mostrarActividades();
+            location.reload();
+        });
+    } else if (indexActividad == 1) {
+        let formularioModificar = document.forms['formularioActividad'];
+        let descripcionModificar = formularioModificar['descripcion'].value;
+        let notaModificar = formularioModificar['nota'].value;
+
+
+        $.ajax({
+            url: 'http://localhost:8000/modificaractividades' + id,
+            method: 'put',
+            data: {
+                descripcion: descripcionModificar,
+                nota: notaModificar,
+            }
+        }).done(response => {
+            const dataJson = JSON.parse(response);
+            const msg = dataJson.data;
+            alert(msg);
+            mostrarActividades();
+            location.reload();
+        });
+    }
+});
+
+let modificar = function (actividadId) {
+    document.getElementById('tituloModal').innerText = 'Modificar';
+    indexActividad = 1;
+    id = actividadId;
+};
+
+let eliminar = function (id) {
+    $.ajax({
+        url: 'http://localhost:8000/eliminaractividades' + id,
+        method: 'delete',
+    }).done(response => {
+        const dataJson = JSON.parse(response);
+        const msg = dataJson.data;
+        alert(msg);
+        mostrarActividades();
+        location.reload();
+    });
+};
